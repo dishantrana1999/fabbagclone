2f506a0c70036eaa0b675c319b9c7d013153bd0b
dishantrana1999 <99647479+dishantrana1999@users.noreply.github.com>
2022-05-08 14:49:54 +0530

    del

--- src/components/LoginSignup/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Home = ()=>{
-    return(
-        <h1>Welcome to Home Page</h1>
-    )
-}
--- src/redux/shopping/shopping-types.js
+++
@@ -1,9 +0,0 @@
-
-
-export const ADD_To_CART ="ADD_TO_CART";
-export const REMOVE_FROM_CART="REMOVE_FROM_CART";
-export const ADJUST_QTY ="ADJUST_QTY";
-export const LOAD_CURRENT_ITEM ="LOAD_CURRENT_ITEM";
-export const CHECKOUT_UPDATE ="CHECKOUT_UPDATE";
-
-
